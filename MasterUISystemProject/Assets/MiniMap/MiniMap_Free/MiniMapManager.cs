fb6e26b2aec9930e23b739a100c1c245ef97e20a
kal5544 <kal5544@psu.edu>
2015-03-18 20:05:50 -0400

    Reorganized Minimap scripts and UI elements

--- MasterUISystemProject/Assets/MiniMap/MiniMap_Free/MiniMapManager.cs
+++ MasterUISystemProject/Assets/MiniMap/MiniMap_Free/MiniMapManager.cs
@@ -6,38 +6,30 @@ public class MiniMapManager : MonoBehaviour {
 
 	public Camera miniMapCam;
 	public GameObject compassPlane;
-	public GameObject avatar;
 	public Text zoomLabel;
-	public Text sizeLabel;
 	public GameObject miniMapCanvas;
 	public RectTransform miniMapRender;
 	public RectTransform miniMapMask;
-
 	public float mapProportionOfScreen = 0.2f;
-	public float maxOrthographicSize = 5;
 	public bool isMiniActive = false;
+	public float orthoCamRadius = 5;
 
-
-	private int zoomLevel = 10;
+	private float orthoCamIncrement;
 	private Vector2 relativeClickPostion = Vector2.zero;
 	private Vector2 miniMapCenter = Vector2.zero;
 	private Vector3 rayStartPostion = Vector2.zero;
 	private int mapDim;
-	private int compassWidth;
-	private int compassHeight;
-
-
+	
 	void Start()
 	{
+		orthoCamIncrement = 0.1f * (float)orthoCamRadius;
 		SetMiniMapCam ();
 		SetCompassTransform ();
-		zoomLabel.text = "Zoom: " + zoomLevel;
-		sizeLabel.text = "Size: " + mapProportionOfScreen;
+		zoomLabel.text = "Radius: " + orthoCamRadius + " meters";
 	}
 
-	void Update(){
-		miniMapCenter = new Vector2 (miniMapMask.position.x - 0.5f*miniMapMask.sizeDelta.x, miniMapMask.position.y -0.5f*miniMapMask.sizeDelta.y);
-
+	void Update()
+	{
 		if(Input.GetKeyDown("m"))
 		{
 			isMiniActive = !isMiniActive;
@@ -49,16 +41,12 @@ public class MiniMapManager : MonoBehaviour {
 
 	public void Teleport()
 	{
-		//Debug.Log("minimap center: " + miniMapCenter);
-		//Debug.Log("Mouse Position: " + Input.mousePosition);
 		relativeClickPostion.x = (Input.mousePosition.x - miniMapCenter.x) / (0.5f*mapDim);
 		relativeClickPostion.y = (Input.mousePosition.y - miniMapCenter.y) / (0.5f*mapDim);
-		//Debug.Log("relative click: " + relativeClickPostion);
 		rayStartPostion = miniMapCam.transform.position + (relativeClickPostion.x*miniMapCam.transform.right + relativeClickPostion.y*miniMapCam.transform.up) * miniMapCam.orthographicSize;
 		RaycastHit hit = new RaycastHit();
 		Physics.Raycast(rayStartPostion,Vector3.down,out hit, Mathf.Infinity);
-		
-		avatar.transform.position = hit.point;
+		POI_ReferenceHub.Instance.Avatar.transform.position = hit.point;
 	}
 
 	void SetCompassTransform()
@@ -69,10 +57,11 @@ public class MiniMapManager : MonoBehaviour {
 
 	void SetMiniMapCam()
 	{
+		miniMapCenter = new Vector2 (miniMapMask.position.x - 0.5f*miniMapMask.sizeDelta.x, miniMapMask.position.y -0.5f*miniMapMask.sizeDelta.y);
 		mapDim = (int)(Screen.height * mapProportionOfScreen);
 		miniMapMask.sizeDelta = new Vector2 (mapDim, mapDim);
 		miniMapRender.sizeDelta = new Vector2 (mapDim, mapDim);
-		miniMapCam.orthographicSize = maxOrthographicSize - maxOrthographicSize * zoomLevel / 100;
+		miniMapCam.orthographicSize = orthoCamRadius;
 	}
 
 	public void ChangeSize(bool isIncrease)
@@ -93,36 +82,21 @@ public class MiniMapManager : MonoBehaviour {
 						mapProportionOfScreen = .9f;
 
 		SetMiniMapCam ();
-
-		sizeLabel.text = "Size: " + mapProportionOfScreen;
 	}
 
 	public void ChangeZoom(bool isIncrease)
 	{
 		if (isIncrease) 
-		{
-			zoomLevel += 10;
-		} 
-		else 
-		{
-			zoomLevel -= 10;
-		}
-		
-		if (zoomLevel > 100) 
-		{
-			zoomLevel =  100;
-		}
+			orthoCamRadius += orthoCamIncrement;
+		else
+			orthoCamRadius -= orthoCamIncrement;
 		
 		// This is where we will reset the zoom of the ortho camera that is used to capture the minimap.
-		float newOrthoSize = maxOrthographicSize - maxOrthographicSize * zoomLevel / 100;
-		if (newOrthoSize <= 0) {
-			newOrthoSize = .01f;
-		}
-		miniMapCam.orthographicSize = newOrthoSize;
+		SetMiniMapCam ();
 		SetCompassTransform ();
 		
 		// set zoom label
-		zoomLabel.text = "Zoom: " + zoomLevel;
+		zoomLabel.text = "Radius: " + orthoCamRadius + " meters";
 	}
 
 }
