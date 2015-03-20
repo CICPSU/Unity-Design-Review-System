303f8c6492035add49007efef69e528fc5b76db6
kal5544 <kal5544@psu.edu>
2015-03-20 11:55:33 -0400

    Fixed bug in Edit mode

--- MasterUISystemProject/Assets/POI Scripts/EditBookmarkBut.cs
+++ MasterUISystemProject/Assets/POI Scripts/EditBookmarkBut.cs
@@ -13,11 +13,11 @@ public class EditBookmarkBut : MonoBehaviour {
 		// Here we get the POI from the POIInfo script that is attached to the activeButton.
 		// We then use this info to populate the edit menu fields.
 		POI clickedPOI = POI_ReferenceHub.Instance.POIMenu.GetComponent<POIActiveButtonManager>().activeButton.GetComponent<POIInfoRef>().poiInfo.Point;
-		POI_ReferenceHub.Instance.POIEditWindow.FindChild("XPosField").FindChild("Text").GetComponent<Text>().text = clickedPOI.position.x.ToString();
-		POI_ReferenceHub.Instance.POIEditWindow.FindChild("YPosField").FindChild("Text").GetComponent<Text>().text = clickedPOI.position.y.ToString();
-		POI_ReferenceHub.Instance.POIEditWindow.FindChild("ZPosField").FindChild("Text").GetComponent<Text>().text = clickedPOI.position.z.ToString();
-		POI_ReferenceHub.Instance.POIEditWindow.FindChild("YRotField").FindChild("Text").GetComponent<Text>().text = clickedPOI.rotation.y.ToString();
-		POI_ReferenceHub.Instance.POIEditWindow.FindChild("NameField").FindChild("Text").GetComponent<Text>().text = clickedPOI.buttonName;
+		POI_ReferenceHub.Instance.poiInfoFields [0].text = clickedPOI.buttonName;
+		POI_ReferenceHub.Instance.poiInfoFields [1].text = clickedPOI.position.x.ToString();
+		POI_ReferenceHub.Instance.poiInfoFields [2].text = clickedPOI.position.y.ToString();
+		POI_ReferenceHub.Instance.poiInfoFields [3].text = clickedPOI.position.z.ToString();
+		POI_ReferenceHub.Instance.poiInfoFields [4].text = clickedPOI.rotation.y.ToString();
 
 		//disable the Add bookmark button
 		POI_ReferenceHub.Instance.POIEditWindow.FindChild("AddBookmark").gameObject.SetActive(false);
