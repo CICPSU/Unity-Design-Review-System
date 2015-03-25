427df587b252d94d3244753c267529fddb3901cf
kal5544 <kal5544@psu.edu>
2015-03-25 18:33:24 -0400

    Added DontDestroyOnLoad object
    Put all permanent objects as children of a DontDestroyOnLoad object so
    that they should persist across scene loads.
    
    Need to test.

---
+++ MasterUISystemProject/Assets/KeepOnLoad.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepOnLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+	}
+}
