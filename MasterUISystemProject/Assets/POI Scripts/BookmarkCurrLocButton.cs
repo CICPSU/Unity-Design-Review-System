2f44fcaec0ca2c94951f2423ff3dc7d07d263468
IConLab <yxl5209@psu.edu>
2015-03-25 21:38:35 -0400

    minor fixes

--- MasterUISystemProject/Assets/POI Scripts/BookmarkCurrLocButton.cs
+++ MasterUISystemProject/Assets/POI Scripts/BookmarkCurrLocButton.cs
@@ -6,14 +6,10 @@ public class BookmarkCurrLocButton : MonoBehaviour {
 
 	public Text posValues;
 
-	public void Update()
-	{
-		posValues.text = POI_ReferenceHub.Instance.Avatar.transform.position.ToString ();
-	}
-
 	public void ToggleBookmarkCurrentWindow()
 	{
 		POI_ReferenceHub.Instance.BookmarkCurrentLocationWindow.gameObject.SetActive (!POI_ReferenceHub.Instance.BookmarkCurrentLocationWindow.gameObject.activeSelf);
 		POI_ReferenceHub.Instance.BookmarkCurrentLocationNameField.GetComponent<InputField> ().text = "";
+		posValues.text = POI_ReferenceHub.Instance.Avatar.transform.position.ToString ();
 	}
 }
