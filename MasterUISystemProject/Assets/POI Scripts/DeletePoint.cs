f025b579e24ef286fb1997a4a4692e74862c2c6d
kal5544 <kal5544@psu.edu>
2015-05-13 10:14:47 -0400

    Delete button fixes
    Deleting a button now disables the EditBookmark and Delete buttons.
    This is more consistent for the control flow.

--- MasterUISystemProject/Assets/POI Scripts/DeletePoint.cs
+++ MasterUISystemProject/Assets/POI Scripts/DeletePoint.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DeletePoint : MonoBehaviour {
@@ -18,6 +19,17 @@ public class DeletePoint : MonoBehaviour {
 		POIButtonManager.instance.RemoveButton(activePOIButton);
 		//close the edit window
 		POI_ReferenceHub.Instance.POIEditWindow.gameObject.SetActive (false);
+
+		//grey out delete point button
+		Transform deleteBut = POI_ReferenceHub.Instance.AddDeleteWindow.FindChild("Delete") as Transform;
+		deleteBut.GetComponent<Button>().enabled = false; //disable delete button
+		Transform deleteButText = deleteBut.FindChild("Text") as Transform;
+		deleteButText.GetComponent<Text>().color = new Color(0.57f,0.57f,0.57f);
+		//grey out edit bookmark
+		Transform editBut = POI_ReferenceHub.Instance.AddDeleteWindow.FindChild("EditBookmark") as Transform;
+		editBut.GetComponent<Button>().enabled = false; //disable edit button
+		Transform editButText = editBut.FindChild("Text") as Transform;
+		editButText.GetComponent<Text>().color = new Color(0.57f,0.57f,0.57f);
 		
 	}
 }
--- MasterUISystemProject/Assets/POI Scripts/NewPoint.cs
+++ MasterUISystemProject/Assets/POI Scripts/NewPoint.cs
@@ -16,6 +16,11 @@ public class NewPoint : MonoBehaviour {
 		deleteBut.GetComponent<Button>().enabled = false; //disable delete button
 		Transform deleteButText = deleteBut.FindChild("Text") as Transform;
 		deleteButText.GetComponent<Text>().color = new Color(0.57f,0.57f,0.57f);
+		//grey out edit bookmark
+		Transform editBut = POI_ReferenceHub.Instance.AddDeleteWindow.FindChild("EditBookmark") as Transform;
+		editBut.GetComponent<Button>().enabled = false; //disable edit button
+		Transform editButText = editBut.FindChild("Text") as Transform;
+		editButText.GetComponent<Text>().color = new Color(0.57f,0.57f,0.57f);
 
 		//clear the old value from input fields
 		foreach(InputField input in POI_ReferenceHub.Instance.poiInfoFields){
