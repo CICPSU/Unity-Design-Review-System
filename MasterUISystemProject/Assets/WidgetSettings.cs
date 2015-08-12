68aeecd04f1479b71021f469955fb8011ac6b6b6
kal5544 <kal5544@psu.edu>
2015-08-11 21:38:40 -0400

    Scriptable object and XML
    Added a separate scene for xml setting serialization.
    
    I think the xml version is going to be more portable/flexible.

---
+++ MasterUISystemProject/Assets/WidgetSettings.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+using System.Xml;
+
+public class WidgetSettings {
+
+	public string gameobjectName;	
+	public float xPos;
+	public float yPos;
+	public float xSize;
+	public float ySize;
+
+	public virtual void ApplySettings()
+	{
+		GameObject gO = GameObject.Find (gameobjectName);
+		RectTransform rectTrans = gO.GetComponent<RectTransform> ();
+		Debug.Log (rectTrans);
+		rectTrans.position = new Vector3 (xPos, yPos, 0);
+		rectTrans.sizeDelta = new Vector2 (xSize, ySize);
+	}
+
+	public WidgetSettings()
+	{
+		gameobjectName = "name";
+	}
+
+}
