81307dbd49e9b21e201b57603edca3d68d3fcabd
kal5544 <kal5544@psu.edu>
2015-12-14 22:15:27 -0500

    Updated Carl prefab
    Now when the Carl prefab is dropped in, he wanders around the scene.  He
    is set to Local wander, but that can be changed to Idle or World in the
    prefab.

---
+++ MasterUISystemProject/Assets/AnimatedCharacters/Scripts/NavMeshWander.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshWander : MonoBehaviour {
+
+    public enum WanderMode { World, Local, Idle}
+
+    public float localWanderRadius = 10;
+
+    public Vector3 localWanderCenter = Vector3.zero;
+
+    public GameObject localWanderCenterRef;
+
+    public WanderMode mode = WanderMode.Local;
+
+    private NavMeshAgent navAgent;
+
+    private Animator animator;
+
+    private NavMeshHit hit;
+
+	// Use this for initialization
+	void Start () {
+        if (localWanderCenterRef != null)
+            localWanderCenter = localWanderCenterRef.transform.position;
+        navAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        ConfigureDestination();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (navAgent.remainingDistance < 1)
+            ConfigureDestination();
+	}
+
+    public void ConfigureDestination()
+    {
+        if (mode == WanderMode.Idle)
+        {
+            navAgent.Stop();
+            animator.SetFloat("Speed", 0f);
+        }
+        else if (mode == WanderMode.Local)
+        {
+            NavMesh.SamplePosition(localWanderCenter + new Vector3(Random.Range(-localWanderRadius, localWanderRadius), 0, Random.Range(-localWanderRadius, localWanderRadius)), out hit, 10, -1);
+            navAgent.SetDestination(hit.position);
+            navAgent.Resume();
+            animator.SetFloat("Speed", 1f);
+        }
+        else
+        {
+            NavMesh.SamplePosition(transform.position + new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), out hit, 10, -1);
+            navAgent.SetDestination(hit.position);
+            navAgent.Resume();
+            animator.SetFloat("Speed", 1f);
+        }
+    }
+}
