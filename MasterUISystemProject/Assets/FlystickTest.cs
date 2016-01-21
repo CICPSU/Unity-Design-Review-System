143da4e804d445fb46a676d4041fbd9442198cfa
kal5544 <kal5544@psu.edu>
2016-01-21 09:13:41 -0600

    Flystick input and test
    Added in a flystick input file and a test object in the scene to test
    the inputs in the editor.  Currently the values of the input should
    appear in the editor window for the FlystickTest script on the
    FlystickTest object.

---
+++ MasterUISystemProject/Assets/FlystickTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlystickTest : MonoBehaviour {
+
+    public float axisZero;
+    public float axisOne;
+
+    public bool buttonZero;
+    public bool buttonOne;
+    public bool buttonTwo;
+    public bool buttonThree;
+    public bool buttonFour;
+    public bool buttonFive;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        axisZero = (float)InputManager.map.GetValue("Axis0");
+        axisOne = (float)InputManager.map.GetValue("Axis1");
+
+        buttonZero = (bool)InputManager.map.GetValue("Button0");
+        buttonOne = (bool)InputManager.map.GetValue("Button1");
+        buttonTwo = (bool)InputManager.map.GetValue("Button2");
+        buttonThree = (bool)InputManager.map.GetValue("Button3");
+        buttonFour = (bool)InputManager.map.GetValue("Button4");
+        buttonFive = (bool)InputManager.map.GetValue("Button5");
+
+    }
+}
