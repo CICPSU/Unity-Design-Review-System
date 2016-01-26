b7489c3693c1232a66c9fc89b5180d8b1a068140
kal5544 <kal5544@psu.edu>
2016-01-26 17:54:24 -0600

    Fixed Marker Info canvas position bug
    Marker Info panel is now positioned properly.

--- MasterUISystemProject/Assets/FullPackage/Widgets/ShowObjInfo/MarkerInfoCanvasSetup.cs
+++ MasterUISystemProject/Assets/FullPackage/Widgets/ShowObjInfo/MarkerInfoCanvasSetup.cs
@@ -11,34 +11,11 @@ public class MarkerInfoCanvasSetup : MonoBehaviour {
 	}
 
 	public void OnMouseUp()
-	{
-		if(gameObject.name == "Pin")//pin head clicked
-        { 
-		    markerInfoCanvasRef.SetActive (true);
-            markerInfoCanvasRef.transform.GetChild(0).transform.position = Input.mousePosition; 
-            /*
-		    markerInfoCanvasRef.GetComponent <RectTransform>().position = gameObject.transform.position + new Vector3(.25f,1,0);
-		    markerInfoCanvasRef.GetComponent <RectTransform>().LookAt (DIRE.Instance.Head.transform);
-		    markerInfoCanvasRef.transform.RotateAround (markerInfoCanvasRef.transform.position, markerInfoCanvasRef.transform.up, 180);
-		    markerInfoCanvasRef.GetComponent <RectTransform>().position -= markerInfoCanvasRef.GetComponent <RectTransform>().forward*0.5f;
-            */
-		    markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().nameText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().name;
-		    markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().positionText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().position.ToString();
-		}
-        else // needle clicked
-        { 
-			markerInfoCanvasRef.SetActive (true);
-            markerInfoCanvasRef.transform.GetChild(0).transform.position = Input.mousePosition;
-            /*
-			markerInfoCanvasRef.GetComponent <RectTransform>().position = gameObject.transform.parent.position + new Vector3(.25f,1,0);
-			markerInfoCanvasRef.GetComponent <RectTransform>().LookAt (DIRE.Instance.Head.transform);
-			markerInfoCanvasRef.transform.RotateAround (markerInfoCanvasRef.transform.position, markerInfoCanvasRef.transform.up, 180);
-			markerInfoCanvasRef.GetComponent <RectTransform>().position -= markerInfoCanvasRef.GetComponent <RectTransform>().forward*0.5f;
-			*/
-            markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().nameText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().name;
-			markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().positionText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().position.ToString();
-		}
+    { 
+		markerInfoCanvasRef.SetActive (true);
+        markerInfoCanvasRef.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition = Input.mousePosition;
+        markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().nameText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().name;
+		markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().positionText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().position.ToString();
+		
 	}
-
-
 }
