4eff900838cbf7cb1188b9037e14c643d812a3e7
kal5544 <kal5544@psu.edu>
2016-01-26 10:58:28 -0600

    Marker Info and Character Options
    Changed the Marker Info canvas to screen space.  Removed the close
    button from the character options panel.

--- MasterUISystemProject/Assets/FullPackage/Widgets/ShowObjInfo/MarkerInfoCanvasSetup.cs
+++ MasterUISystemProject/Assets/FullPackage/Widgets/ShowObjInfo/MarkerInfoCanvasSetup.cs
@@ -12,23 +12,30 @@ public class MarkerInfoCanvasSetup : MonoBehaviour {
 
 	public void OnMouseUp()
 	{
-		if(gameObject.name == "Pin"){ //pin head clicked
-		markerInfoCanvasRef.SetActive (true);
-		markerInfoCanvasRef.GetComponent <RectTransform>().position = gameObject.transform.position + new Vector3(.25f,1,0);
-		markerInfoCanvasRef.GetComponent <RectTransform>().LookAt (DIRE.Instance.Head.transform);
-		markerInfoCanvasRef.transform.RotateAround (markerInfoCanvasRef.transform.position, markerInfoCanvasRef.transform.up, 180);
-		markerInfoCanvasRef.GetComponent <RectTransform>().position -= markerInfoCanvasRef.GetComponent <RectTransform>().forward*0.5f;
-
-		markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().nameText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().name;
-		markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().positionText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().position.ToString();
-		}else{ // needle clicked
+		if(gameObject.name == "Pin")//pin head clicked
+        { 
+		    markerInfoCanvasRef.SetActive (true);
+            markerInfoCanvasRef.transform.GetChild(0).transform.position = Input.mousePosition; 
+            /*
+		    markerInfoCanvasRef.GetComponent <RectTransform>().position = gameObject.transform.position + new Vector3(.25f,1,0);
+		    markerInfoCanvasRef.GetComponent <RectTransform>().LookAt (DIRE.Instance.Head.transform);
+		    markerInfoCanvasRef.transform.RotateAround (markerInfoCanvasRef.transform.position, markerInfoCanvasRef.transform.up, 180);
+		    markerInfoCanvasRef.GetComponent <RectTransform>().position -= markerInfoCanvasRef.GetComponent <RectTransform>().forward*0.5f;
+            */
+		    markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().nameText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().name;
+		    markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().positionText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().position.ToString();
+		}
+        else // needle clicked
+        { 
 			markerInfoCanvasRef.SetActive (true);
+            markerInfoCanvasRef.transform.GetChild(0).transform.position = Input.mousePosition;
+            /*
 			markerInfoCanvasRef.GetComponent <RectTransform>().position = gameObject.transform.parent.position + new Vector3(.25f,1,0);
 			markerInfoCanvasRef.GetComponent <RectTransform>().LookAt (DIRE.Instance.Head.transform);
 			markerInfoCanvasRef.transform.RotateAround (markerInfoCanvasRef.transform.position, markerInfoCanvasRef.transform.up, 180);
 			markerInfoCanvasRef.GetComponent <RectTransform>().position -= markerInfoCanvasRef.GetComponent <RectTransform>().forward*0.5f;
-			
-			markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().nameText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().name;
+			*/
+            markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().nameText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().name;
 			markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().positionText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().position.ToString();
 		}
 	}
