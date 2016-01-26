b71b5697fca94d07779ff854254482b2bdd661b9
kal5544 <kal5544@psu.edu>
2016-01-26 17:36:15 -0600

    TimeScale example
    Created a sample scene that shows how altering Time.timeScale affects
    physics and animations.  Press Space to cycle through values from 1 to
    0.

---
+++ MasterUISystemProject/Assets/TimeScaleTest/TimeScalePrint.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeScalePrint : MonoBehaviour {
+
+    public Text timeScaleText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (Time.timeScale == 0)
+                Time.timeScale = 1f;
+            else if (Time.timeScale < .1f)
+                Time.timeScale = 0f;
+            else
+                Time.timeScale -= .1f;
+
+           
+        }
+        timeScaleText.text = "TimeScale = " + Time.timeScale;
+	}
+}
