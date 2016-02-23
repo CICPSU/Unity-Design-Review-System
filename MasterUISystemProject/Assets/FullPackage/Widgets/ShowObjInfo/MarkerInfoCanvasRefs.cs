7cea0206298efb42e5d1bc34f68449cf621640ef
kal5544 <kal5544@psu.edu>
2016-02-23 14:26:02 -0600

    Updated the Markers from POI
    The markers from poi now use the RaycastLock to prevent opening their
    menu when theyre not suppose to

--- MasterUISystemProject/Assets/FullPackage/Widgets/ShowObjInfo/MarkerInfoCanvasRefs.cs
+++ MasterUISystemProject/Assets/FullPackage/Widgets/ShowObjInfo/MarkerInfoCanvasRefs.cs
@@ -4,4 +4,13 @@ using System.Collections;
 public class MarkerInfoCanvasRefs : MonoBehaviour {
 
 	public GameObject nameText, positionText;
+    public static GameObject activeMarker;
+
+    public void CloseMarkerInfo()
+    {
+        activeMarker.GetComponent<MarkerInfoCanvasSetup>().hasRaycastLock = false;
+        RaycastLock.GiveLock();
+        gameObject.SetActive(false);
+
+    }
 }
--- MasterUISystemProject/Assets/FullPackage/Widgets/ShowObjInfo/MarkerInfoCanvasSetup.cs
+++ MasterUISystemProject/Assets/FullPackage/Widgets/ShowObjInfo/MarkerInfoCanvasSetup.cs
@@ -5,17 +5,23 @@ using System.Collections;
 public class MarkerInfoCanvasSetup : MonoBehaviour {
 	private GameObject markerInfoCanvasRef;
 
+    public bool hasRaycastLock = false;
+
 	void Start()
 	{
 		markerInfoCanvasRef = POI_ReferenceHub.Instance.markerInfoCanvas;
 	}
 
 	public void OnMouseUp()
-    { 
-		markerInfoCanvasRef.SetActive (true);
-        markerInfoCanvasRef.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition = Input.mousePosition;
-        markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().nameText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().name;
-		markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs> ().positionText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo> ().position.ToString();
-		
+    {
+        if (!hasRaycastLock && RaycastLock.GetLock())
+        {
+            hasRaycastLock = true;
+            MarkerInfoCanvasRefs.activeMarker = gameObject;
+            markerInfoCanvasRef.SetActive(true);
+            markerInfoCanvasRef.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition = Input.mousePosition;
+            markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs>().nameText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo>().name;
+            markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs>().positionText.GetComponent<Text>().text = transform.parent.GetComponent<POIInfo>().position.ToString();
+        }
 	}
 }
