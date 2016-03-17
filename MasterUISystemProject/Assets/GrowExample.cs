d3da400a8b3b4310147c9f5d3bc70b36887e4f6e
kal5544 <kal5544@psu.edu>
2016-03-17 13:09:19 -0500

    Added UI examples to the sample iTween scene
    Added fade, grow and slide to the itween sample scene in the plugins
    folder.

---
+++ MasterUISystemProject/Assets/GrowExample.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowExample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        iTween.ScaleBy(gameObject, iTween.Hash("x", 0, "y", 0, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
