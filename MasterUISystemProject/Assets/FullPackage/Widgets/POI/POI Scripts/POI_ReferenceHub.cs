3432a722c760429fe6d7c28be7ea5f19372691c9
kal5544 <kal5544@psu.edu>
2016-05-17 17:17:08 -0400

    Added in POIEdit window Open/Close functions
    Added functions to contain the code for Opening and Closing the POIEdit
    window.
    
    Also added in code for filling in the POIInfo fields so that there
    wouldn't be issues with fields being out of order or incorrectly set.
    
    Fixed positioning of the POIEditWindow.

--- MasterUISystemProject/Assets/FullPackage/Widgets/POI/POI Scripts/POI_ReferenceHub.cs
+++ MasterUISystemProject/Assets/FullPackage/Widgets/POI/POI Scripts/POI_ReferenceHub.cs
@@ -50,4 +50,49 @@ public class POI_ReferenceHub : MonoBehaviour{
 		defaultMarkerPrefab = Resources.Load ("POIPanel/Pin");
 	}
 	
+    public void OpenEditWindow()
+    {
+        if (!Instance.POIEditWindow.gameObject.activeSelf)
+            Instance.POIEditWindow.gameObject.SetActive(true);
+
+        // Here we get the POI from the POIInfo script that is attached to the activeButton.
+        // We then use this info to populate the edit menu fields.
+        POI clickedPOI = Instance.POIMenu.GetComponent<POIActiveButtonManager>().activeButton.GetComponent<POIInfoRef>().poiInfo.Point;
+        Instance.poiInfoFields[0].text = clickedPOI.buttonName;
+        Instance.poiInfoFields[1].text = clickedPOI.position.x.ToString();
+        Instance.poiInfoFields[2].text = clickedPOI.position.y.ToString();
+        Instance.poiInfoFields[3].text = clickedPOI.position.z.ToString();
+        Instance.poiInfoFields[4].text = clickedPOI.rotation.y.ToString();
+
+        //disable the Add bookmark button, enable save changes
+        Instance.POIEditWindow.FindChild("AddBookmark").gameObject.SetActive(false);
+        Instance.POIEditWindow.FindChild("SaveChanges").gameObject.SetActive(true);
+
+        //grey out edit bookmark
+        Transform editBut = Instance.AddDeleteWindow.FindChild("EditBookmark") as Transform;
+        editBut.GetComponent<Button>().enabled = false; //disable edit button
+        Transform editButText = editBut.FindChild("Text") as Transform;
+        editButText.GetComponent<Text>().color = new Color(0.57f, 0.57f, 0.57f);
+    }
+
+    public void CloseEditWindow()
+    {
+        //enable edit bookmark
+        Transform editBut = Instance.AddDeleteWindow.FindChild("EditBookmark") as Transform;
+        editBut.GetComponent<Button>().enabled = true; //enable edit button
+        Transform editButText = editBut.FindChild("Text") as Transform;
+        editButText.GetComponent<Text>().color = new Color(50f / 255, 50f / 255, 50f / 255);
+
+        Instance.POIEditWindow.gameObject.SetActive(false);
+    }
+
+    public void FillPOIInfoFields(POI newPoint)
+    {
+        Instance.poiInfoFields[0].text = newPoint.buttonName;
+        Instance.poiInfoFields[1].text = newPoint.position.x.ToString();
+        Instance.poiInfoFields[2].text = newPoint.position.y.ToString();
+        Instance.poiInfoFields[3].text = newPoint.position.z.ToString();
+        Instance.poiInfoFields[4].text = newPoint.rotation.y.ToString();
+    }
+
 }
