867e50f1b3bca9c956182758ab2260e38d3840ac
Yifan Liu <yxl5209@psu.edu>
2016-05-17 12:11:19 -0400

    UI upgrade test

---
+++ MasterUISystemProject/Assets/CurvedUI/Demo Scenes/Demo Scenes Content/CUI_GunMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CUI_GunMovement : MonoBehaviour {
+
+	[SerializeField] Transform pivot;
+	[SerializeField]  float sensitivity = 0.1f;
+	Vector3 lastMouse;
+
+	// Use this for initialization
+	void Start () {
+		lastMouse = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 mouseDelta = Input.mousePosition - lastMouse;
+		lastMouse = Input.mousePosition;
+		pivot.localEulerAngles += new Vector3 (-mouseDelta.y,  mouseDelta.x, 0) * sensitivity;
+
+	}
+}
