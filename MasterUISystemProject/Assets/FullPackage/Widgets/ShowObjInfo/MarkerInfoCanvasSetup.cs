90459d63e54adbebdaaea4d82b58a87a10ff31ef
kal5544 <kal5544@psu.edu>
2016-06-11 10:18:22 -0400

    Fixed MarkerInfoCanvas Delete
    Fixed the Delete button on the MarkerInfoCanvas so that it properly
    deletes the markers now.
    
    The hierarchy of the pin changed so the reference to the POIInfo script
    changed.

--- MasterUISystemProject/Assets/FullPackage/Widgets/ShowObjInfo/MarkerInfoCanvasSetup.cs
+++ MasterUISystemProject/Assets/FullPackage/Widgets/ShowObjInfo/MarkerInfoCanvasSetup.cs
@@ -28,7 +28,7 @@ public class MarkerInfoCanvasSetup : MonoBehaviour {
 
     public void SetupCanvas()
     {
-        MarkerInfoCanvasRefs.activeMarker = gameObject.transform.parent.gameObject;
+        MarkerInfoCanvasRefs.activeMarker = gameObject.transform.parent.parent.gameObject;
         markerInfoCanvasRef.SetActive(true);
         markerInfoCanvasRef.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition = UIUtilities.SetPopUpPanel(markerInfoCanvasRef.transform.GetChild(0).GetComponent<RectTransform>());
         markerInfoCanvasRef.GetComponent<MarkerInfoCanvasRefs>().nameText.GetComponent<Text>().text = transform.parent.parent.GetComponent<POIInfo>().name;
