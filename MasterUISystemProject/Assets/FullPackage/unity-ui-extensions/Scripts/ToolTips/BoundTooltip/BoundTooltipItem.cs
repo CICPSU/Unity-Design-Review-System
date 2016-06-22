e85c25ad7ff8b3ac0665ebf0aad1ad2f29652e8f
kal5544 <kal5544@psu.edu>
2016-06-22 09:15:48 -0400

    Added some comments to BoundTooltipItem

--- MasterUISystemProject/Assets/FullPackage/unity-ui-extensions/Scripts/ToolTips/BoundTooltip/BoundTooltipItem.cs
+++ MasterUISystemProject/Assets/FullPackage/unity-ui-extensions/Scripts/ToolTips/BoundTooltip/BoundTooltipItem.cs
@@ -24,17 +24,26 @@ namespace UnityEngine.UI.Extensions
             HideTooltip();
         }
 
+        /// <summary>
+        /// this function is called from the TooltipTrigger component
+        /// this function displays the tooltip and sizes it to fit the text inside
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="pos"></param>
         public void ShowTooltip(string text, Vector3 pos)
         {
             if (TooltipText.text != text)
                 TooltipText.text = text;
             Vector3 finalPos = pos + ToolTipOffset;
+            
+            // this code is used to keep the tooltip on the screen, but causes flickering when it moves the tooltip under the mouse
+            /*
             if (finalPos.y + .5f * GetComponent<RectTransform>().sizeDelta.y > Screen.height)
                 finalPos -= new Vector3(0, finalPos.y + .5f * GetComponent<RectTransform>().sizeDelta.y - Screen.height,0);
             if (finalPos.x + .5f * GetComponent<RectTransform>().sizeDelta.x > Screen.width)
                 finalPos -= new Vector3(0, finalPos.x + .5f * GetComponent<RectTransform>().sizeDelta.x - Screen.width, 0);
-
-            transform.position = pos + ToolTipOffset;
+                */
+            transform.position = finalPos;
 
             gameObject.SetActive(true);
             GetComponent<RectTransform>().sizeDelta = new Vector2(LayoutUtility.GetPreferredWidth(TooltipText.rectTransform) + 100 ,GetComponent<RectTransform>().sizeDelta.y);
