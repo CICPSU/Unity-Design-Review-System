1eee60c8a279c18f53320125e37bbb1608cb5954
kal5544 <kal5544@psu.edu>
2016-07-19 11:13:29 -0400

    Added comments to RaycastControl

--- MasterUISystemProject/Assets/FullPackage/Scripts/RaycastControl.cs
+++ MasterUISystemProject/Assets/FullPackage/Scripts/RaycastControl.cs
@@ -23,22 +23,44 @@ public class RaycastControl : MonoBehaviour{
         }
     }
     
+    /// <summary>
+    /// This function performs a raycast from the cursor's location.
+    /// The ignoreLayers argument allows the user to specify what layers the raycast should ignore.
+    /// </summary>
+    /// <param name="ignoreLayers"></param>
     public static void RaycastCursor(int ignoreLayers)
     {
         mouseCam = FindMouseCamera();
         Raycast(mouseCam.ScreenPointToRay(Input.mousePosition), ignoreLayers);
     }
 
+    /// <summary>
+    /// This is a function wrapper for the Physics.Raycast function.
+    /// The wrapper is used so that the data is stored in RaycastControl as hit.
+    /// </summary>
+    /// <param name="startPosition"></param>
+    /// <param name="direction"></param>
+    /// <param name="ignoreLayers"></param>
     public static void Raycast(Vector3 startPosition, Vector3 direction, int ignoreLayers)
     {
         Physics.Raycast(startPosition, direction, out hit, 1000, ignoreLayers);
     }
 
+    /// <summary>
+    /// A wrapped like above, but for a different function signature.
+    /// </summary>
+    /// <param name="ray"></param>
+    /// <param name="ignoreLayers"></param>
     public static void Raycast(Ray ray, int ignoreLayers)
     {
         Physics.Raycast(ray, out hit, 1000, ignoreLayers);
     }
 
+    /// <summary>
+    /// This function is used to determine which camera's viewport the cursor is currently in.
+    /// This is used to make the raycast through the correct camera.
+    /// </summary>
+    /// <returns></returns>
     public static Camera FindMouseCamera()
     {
         List<Camera> camList = (from cam in FindObjectsOfType<Camera>() where cam.targetTexture == null select cam).ToList();
